5efc37dfd7bd897c0f212cd8187fb15ae5bdeabb
Sergio Joya <284065931+sej-4@users.noreply.github.com>
2026-07-02 17:41:10 -0400

    Add function to play a single round

--- script.js
+++ script.js
@@ -15,3 +15,29 @@ function getComputerChoice() {
 function getHumanChoice() {
   return prompt("Enter your choice of rock, papers, or scissors:");
 }
+
+function capitalize(string) {
+  return `${string.at(0).toUpperCase()}${string.slice(1).toLowerCase()}`;
+}
+
+function playRound(humanChoice, computerChoice) {
+  humanChoice = humanChoice.toLowerCase();
+
+  if (humanChoice === computerChoice) {
+    console.log("It's a tie! Play again");
+  } else if (
+    (humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "scissors" && computerChoice === "paper") ||
+    (humanChoice === "paper" && computerChoice === "rock")
+  ) {
+    console.log(
+      `You win! ${capitalize(humanChoice)} beats ${capitalize(computerChoice)}`,
+    );
+    humanScore++;
+  } else {
+    console.log(
+      `You lose! ${capitalize(computerChoice)} beats ${capitalize(humanChoice)}`,
+    );
+    computerScore++;
+  }
+}
