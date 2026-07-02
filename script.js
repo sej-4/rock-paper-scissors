7240e658ba93878ce0b1187c863c2c1e6abe296d
Sergio Joya <284065931+sej-4@users.noreply.github.com>
2026-07-02 14:19:05 -0400

    Add function to get computer choice

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("Hello");
+function getComputerChoice() {
+  const randomNumber = Math.floor(Math.random() * 3);
+  if (randomNumber === 0) {
+    return "rock";
+  } else if (randomNumber === 1) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+}
