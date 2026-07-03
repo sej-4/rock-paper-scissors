0a5ab95dc26ac4c386c05e9c11358cd55b1be02e
Sergio Joya <284065931+sej-4@users.noreply.github.com>
2026-07-03 12:39:00 -0400

    Refactor getComputerChoice and getHumanChoice

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 function getComputerChoice() {
   const randomNumber = Math.floor(Math.random() * 3);
-  if (randomNumber === 0) {
-    return "rock";
-  } else if (randomNumber === 1) {
-    return "paper";
-  } else {
-    return "scissors";
+
+  switch (randomNumber) {
+    case 0:
+      return "rock";
+    case 1:
+      return "paper";
+    case 2:
+      return "scissors";
   }
 }
 
 function getHumanChoice() {
-  return prompt("Enter your choice of rock, papers, or scissors:");
+  return prompt("Enter rock, papers, or scissors:");
 }
 
 function capitalize(string) {
