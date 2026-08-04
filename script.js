674b6a4655b23333e865a41f49ec4d66cf216e6f
Sergio Joya <284065931+sej-4@users.noreply.github.com>
2026-08-04 14:51:06 -0400

    Refactor functions

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let computerScore = 0;
+let humanScore = 0;
+const buttons = document.querySelectorAll("button");
+
 function getComputerChoice() {
   const randomNumber = Math.floor(Math.random() * 3);
 
@@ -9,10 +13,6 @@ function getComputerChoice() {
     case 2:
       return "scissors";
   }
-}
-
-function capitalize(string) {
-  return `${string.at(0).toUpperCase()}${string.slice(1).toLowerCase()}`;
 }
 
 function getRoundWinner(humanChoice, computerChoice) {
@@ -29,8 +29,54 @@ function getRoundWinner(humanChoice, computerChoice) {
   }
 }
 
-let computerScore = 0;
-let humanScore = 0;
+function capitalize(string) {
+  return `${string.at(0).toUpperCase()}${string.slice(1).toLowerCase()}`;
+}
+
+function displayRoundWinner(roundWinner, humanChoice, computerChoice) {
+  const result = document.querySelector("#result");
+
+  if (roundWinner === "human") {
+    result.textContent = `You win! ${capitalize(humanChoice)} beats ${capitalize(computerChoice)}`;
+  } else if (roundWinner === "computer") {
+    result.textContent = `You lose! ${capitalize(computerChoice)} beats ${capitalize(humanChoice)}`;
+  } else {
+    result.textContent = "It's a tie!";
+  }
+}
+
+function updateScore(roundWinner) {
+  if (roundWinner === "human") humanScore++;
+  if (roundWinner === "computer") computerScore++;
+}
+
+function displayScore() {
+  const score = document.querySelector("#score");
+  score.textContent = `Human: ${humanScore} Computer: ${computerScore}`;
+}
+
+function checkGameOver() {
+  return humanScore === 5 || computerScore === 5;
+}
+
+function displayGameWinner() {
+  const div = document.querySelector("div");
+  const gameWinner = document.createElement("p");
+
+  gameWinner.textContent =
+    humanScore > computerScore
+      ? "Game Over! You won the game"
+      : "Game Over! Computer won the game";
+
+  gameWinner.style.backgroundColor = "yellow";
+  div.append(gameWinner);
+}
+
+function disableButtons() {
+  buttons.forEach((button) => {
+    button.disabled = true;
+  });
+}
 
 function playRound(humanChoice, computerChoice) {
   const roundWinner = getRoundWinner(humanChoice, computerChoice);
@@ -45,8 +91,6 @@ function playRound(humanChoice, computerChoice) {
   }
 }
 
-const buttons = document.querySelectorAll("button");
-
 buttons.forEach((button) => {
   button.addEventListener("click", (event) => {
     const humanChoice = event.target.id;
@@ -54,54 +98,3 @@ buttons.forEach((button) => {
     playRound(humanChoice, computerChoice);
   });
 });
-
-function checkGameOver() {
-  return humanScore === 5 || computerScore === 5;
-}
-
-function displayGameWinner() {
-  const div = document.querySelector("div");
-  const gameWinner = document.createElement("p");
-  gameWinner.textContent =
-    humanScore > computerScore
-      ? "Game Over! You won the game"
-      : "Game Over! Computer won the game";
-  gameWinner.style.backgroundColor = "yellow";
-  div.append(gameWinner);
-}
-
-function disableButtons() {
-  const buttons = document.querySelectorAll("button");
-  buttons.forEach((button) => {
-    button.disabled = true;
-  });
-}
-
-function displayRoundWinner(roundWinner, humanChoice, computerChoice) {
-  const div = document.querySelector("div");
-  const result = document.querySelector("#result");
-
-  if (roundWinner === "human") {
-    result.textContent = `You win! ${capitalize(humanChoice)} beats ${capitalize(computerChoice)}`;
-  } else if (roundWinner === "computer") {
-    result.textContent = `You lose! ${capitalize(computerChoice)} beats ${capitalize(humanChoice)}`;
-  } else {
-    result.textContent = "It's a tie!";
-  }
-
-  div.appendChild(result);
-}
-
-function updateScore(roundWinner) {
-  if (roundWinner === "human") {
-    humanScore++;
-  } else if (roundWinner === "computer") {
-    computerScore++;
-  }
-}
-
-function displayScore() {
-  const div = document.querySelector("div");
-  const score = document.querySelector("#score");
-  score.textContent = `Human: ${humanScore} Computer: ${computerScore}`;
-}
